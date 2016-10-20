28f7fe34eb42a878146c6a10609ca6951269d77c
Jimit Bagadiya <socialpilot@Jimits-Mac-mini.local>
2016-10-20 10:36:02 +0530

    Initial Commit

---
+++ Tapovan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tapovan
+//
+//  Created by Jimit Bagadiya on 20/10/16.
+//  Copyright © 2016 Jimit Bagadiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tapovan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tapovan
+//
+//  Created by Jimit Bagadiya on 20/10/16.
+//  Copyright © 2016 Jimit Bagadiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
